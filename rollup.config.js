754bcdaa17e6dad9fd2804251151b45eb6a42b02
Gerald <code@gera2ld.space>
2023-12-08 18:33:14 +0800

    chore: update build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,69 +1,18 @@
-import plaid from '@gera2ld/plaid';
+import { defineConfig } from 'rollup';
+import { definePlugins, defineExternal } from '@gera2ld/plaid-rollup';
 import pkg from './package.json' assert { type: 'json' };
 
-const {
-  defaultOptions,
-  getRollupExternal,
-  getRollupPlugins,
-} = plaid;
-
-const DIST = defaultOptions.distDir;
-const BANNER = `/*! ${pkg.name} v${pkg.version} | ${pkg.license} License */`;
-
-const external = Object.keys(pkg.dependencies);
-const bundleOptions = {
-  extend: true,
-  esModule: false,
-};
-const rollupConfig = [
-  {
-    input: 'src/bin.ts',
-    plugins: getRollupPlugins({
-      esm: true,
-      extensions: defaultOptions.extensions,
-      minimize: false,
-      aliases: {
-        entries: {
-          '.': './index.mjs',
-        },
-      },
-    }),
-    external: getRollupExternal([
-      ...external,
-      (id) => {
-        if (id === './index.mjs') return true;
-        return false;
-      },
-    ]),
-    output: {
-      format: 'esm',
-      file: `${DIST}/bin.mjs`,
-      banner: '#!/usr/bin/env node'
-    },
+export default defineConfig({
+  input: {
+    bin: 'src/bin.ts',
+    index: 'src/index.ts',
   },
-  {
-    input: 'src/index.ts',
-    plugins: getRollupPlugins({
-      esm: true,
-      extensions: defaultOptions.extensions,
-      minimize: false,
-    }),
-    external: getRollupExternal(external),
-    output: {
-      format: 'esm',
-      file: `${DIST}/index.mjs`,
-      banner: BANNER,
-    },
+  plugins: definePlugins({
+    esm: true,
+    minimize: false,
+  }),
+  external: defineExternal(Object.keys(pkg.dependencies)),
+  output: {
+    dir: 'dist',
   },
-];
-
-rollupConfig.forEach((item) => {
-  item.output = {
-    indent: false,
-    // If set to false, circular dependencies and live bindings for external imports won't work
-    externalLiveBindings: false,
-    ...item.output,
-  };
 });
-
-export default rollupConfig;
